cb1f0ce4d58b8ac3a238cb485ba25b2e95274aa4
lump3n <bul.reyn@gmail.com>
2024-05-11 14:46:25 +0300

    обновление конфига для коллаба

---
+++ visualization.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+
+def plot_pcb_with_bndboxs(labels, boxes: str, image_path: str, ):
+    # image = image = plt.imread(image_path)
+
+    pass
+
+    # plt.imshow(image)
+    # plt.axis('off')
+    # plt.show()
--- yolov8/yolo_config.py
+++ yolov8/yolo_config.py
@@ -1,12 +1,18 @@
 TARGET_SIZE: int = 640
 
-ORIGINAL_DATA_DIR = '../original_data/'
+# for colab
+# ORIGINAL_DATA_DIR: str = '/content/drive/MyDrive/VKRM/pcb_defect_detection/original_data/'
+# ORIGINAL_DATA_IMG_DIR: str = '/content/drive/MyDrive/VKRM/pcb_defect_detection/original_data/images/'
+# ORIGINAL_DATA_ANNO_DIR: str = '/content/drive/MyDrive/VKRM/pcb_defect_detection/original_data/annotations/'
+
+
+ORIGINAL_DATA_DIR: str = '../original_data/'
 ORIGINAL_DATA_IMG_DIR: str = '../original_data/images/'
 ORIGINAL_DATA_ANNO_DIR: str = '../original_data/annotations/'
 ORIGINAL_DATA_SUBDIRS: list[str] = ['spurious_copper/', 'mouse_bite/', 'open_circuit/', 'missing_hole/', 'spur/', 'short/']
 
 
-DATA_DIR = 'datasets/data/'
+DATA_DIR: str = 'datasets/data/'
 TRAIN_IMG_DIR: str = 'datasets/data/images/train/'
 TRAIN_ANNO_DIR: str = 'datasets/data/labels/train/'
 VAL_IMG_DIR: str = 'datasets/data/images/val/'
