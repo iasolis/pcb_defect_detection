9293a889559df1ba6c1024e9183b2702e2aa3b7b
lump3n <bul.reyn@gmail.com>
2024-05-17 12:11:57 +0300

    Изменил класс

--- rcnn/train.py
+++ rcnn/train.py
@@ -18,8 +18,6 @@ def collate_fn(batch):
     return tuple(zip(*batch))
 
 
-
-
 def train_one_epoch(model, optimizer, lr_scheduler, train_loader, val_loader, device, epoch):
     model.to(device)
     model.train()
@@ -50,35 +48,35 @@ def train_one_epoch(model, optimizer, lr_scheduler, train_loader, val_loader, de
         if lr_scheduler is not None:
             lr_scheduler.step()
 
-        # validation
-        model.eval()
-        with torch.no_grad():
-            tk = tqdm(val_loader)
-            for images, targets in tk:
-                images = list(image.to(device) for image in images)
-                targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-                val_output = model(images)
-                val_output = [{k: v.to(device) for k, v in t.items()} for t in val_output]
-                IOU = []
-                for j in range(len(val_output)):
-                    a, b = val_output[j]['boxes'].cpu().detach(), targets[j]['boxes'].cpu().detach()
-                    chk = torchvision.ops.box_iou(a, b)
-                    res = np.nanmean(chk.sum(axis=1) / (chk > 0).sum(axis=1))
-                    IOU.append(res)
-                tk.set_postfix(IoU=np.mean(IOU))
-            tk.close()
-
-
+    # validation
+    model.eval()
+    with torch.no_grad():
+        tk = tqdm(val_loader)
+        for images, targets in tk:
+            images = list(image.to(device) for image in images)
+            targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+            val_output = model(images)
+            val_output = [{k: v.to(device) for k, v in t.items()} for t in val_output]
+            IOU = []
+            for j in range(len(val_output)):
+                a, b = val_output[j]['boxes'].cpu().detach(), targets[j]['boxes'].cpu().detach()
+                chk = torchvision.ops.box_iou(a, b)
+                res = np.nanmean(chk.sum(axis=1) / (chk > 0).sum(axis=1))
+                IOU.append(res)
+            tk.set_postfix(IoU=np.mean(IOU))
+        tk.close()
+    IOU_mean = sum(IOU) / len(IOU)
     all_losses_dict = pd.DataFrame(all_losses_dict)  # for printing
     print("Epoch {}, lr: {:.6f}, loss: {:.6f}, loss_classifier: {:.6f}, loss_box: {:.6f},"
-          " loss_rpn_box: {:.6f}, loss_object: {:.6f}".format(
+          " loss_rpn_box: {:.6f}, loss_object: {:.6f}, IOU: {:.6f}".format(
         epoch,
         optimizer.param_groups[0]['lr'],
         np.mean(all_losses),
         all_losses_dict['loss_classifier'].mean(),
         all_losses_dict['loss_box_reg'].mean(),
         all_losses_dict['loss_rpn_box_reg'].mean(),
-        all_losses_dict['loss_objectness'].mean()
+        all_losses_dict['loss_objectness'].mean(),
+        IOU_mean
     ))
 
 
@@ -130,7 +128,7 @@ def main():
         print(var_name, "\t", optimizer.state_dict()[var_name])
 
     for epoch in range(num_epochs):
-        train_one_epoch(model, optimizer, lr_scheduler, train_data_loader, valid_data_loader,  device, epoch)
+        train_one_epoch(model, optimizer, lr_scheduler, train_data_loader, valid_data_loader, device, epoch)
 
 
 if __name__ == '__main__':
